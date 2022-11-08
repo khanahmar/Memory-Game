b52ddb4d8fa1f6c47b87caa1d05ebe57c27fdfaa
Ahmar khan <ahmarbilal223344@gmail.com>
2022-11-08 15:20:37 +0500

    changes made

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 const cards = document.querySelectorAll(".memory-card");
 
 let hasFlippedCard = false;
-let first, secondCard;
+let lockBoard = true;
+let firstCard, secondCard;
 
 function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
   this.classList.toggle("flip");
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  hasFlippedCard = false;
+
+  checkForMatch();
+
+  function checkForMatch() {
+    if (firstCard.dataset.farmework === secondCard.dataset.farmework)
+      disableCards();
+    return;
+  }
+
+  unflipCard();
+  lockBoard = true;
+
+  // Disable Cards function
+  function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+  }
+
+  // Unflip card funciton
+  function unflipCard() {
+    setTimeout(() => {
+      firstCard.classList.remove("flip");
+      secondCard.classList.remove("flip");
+      lockBoard = false;
+    }, 1500);
+  }
 }
 
 cards.forEach((card) => {
